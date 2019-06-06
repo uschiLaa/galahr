7bbeb599cae5bc31eb4e87c7614ffd2f8812466e
Ursula Laa <uschi.laa@gmx.at>
2019-06-06 16:55:03 +1000

    Documentation for data

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Features calculated on different types of time series.
+#'
+#' A dataset containing the features calculated with `tsfeatures`
+#' on three groups of time series: bird songs, air temperature and
+#' music.
+#'
+#' @format A tibble with 1082 rows and 14 variables (13 features and type)
+#' @source \url{https://www.comp-engine.org}
+"tsfeatureData"
+
+#' Scatter points containing a galah.
+#'
+#' Try plotting x vs y or x1 vs y1!
+#'
+#' @format A data frame with 553 rows and 7 variables (6 parameters and color)
+"galah"
+
+#' Posterior samples fitting a gravitational wave observation.
+#'
+#' A dataset containing posterior samples describing neutron star
+#' properties measured by LIGO in the gravitational wave observation
+#' GW170817.
+#'
+#' @format A tibble with 2396 rows and 6 variables
+#' @source \url{https://dcc.ligo.org/public/0152/P1800115/005/Parametrized-EoS_maxmass_posterior_samples.dat}
+"GW170817"
