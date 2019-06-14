17e2cf3d0c33fdb98d86b0b76f109e8c55e68538
Ursula Laa <uschi.laa@gmx.at>
2019-06-14 17:25:44 +0200

    Updating the vignette

---
+++ R/utils-spinebil.R
@@ -0,0 +1,22 @@
+#' Generate basis vector in direction i in n dimensions (i <= n)
+#'
+#' @param i selected direction
+#' @param n number of dimensions
+#' @return basis vector
+#' @export
+basisVector <- function(i, n){
+  v <- rep(0,n)
+  v[i] <- 1
+  v
+}
+
+#' Generate 2-d basis in directions i, j in n dimensions (i,j <= n)
+#'
+#' @param i first basis direction
+#' @param j second basis direction
+#' @param n number of dimensions
+#' @return basis matrix
+#' @export
+basisMatrix <- function(i, j, n){
+  matrix(c(basisVector(i,n), basisVector(j,n)), ncol=2)
+}
